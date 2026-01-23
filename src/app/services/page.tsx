be94ed471decb16b7d158f89339f9cd717c2d3c6
Varun YourLastName <varun@example.com>
2026-01-23 20:08:56 +0530

    services

---
+++ src/app/services/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        <h1>page</h1>
+        <h1>page</h1>
+        <h1>page</h1>
+        <h1>page</h1>
+        <h1>page</h1>
+    </div>
+  )
+}
+
+export default page
