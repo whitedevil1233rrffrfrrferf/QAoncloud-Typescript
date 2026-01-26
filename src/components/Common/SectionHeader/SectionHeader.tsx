b5fe88006d608003a8e26e7ee734f4fd69cf8c47
Varun YourLastName <varun@example.com>
2026-01-26 14:36:12 +0530

    para optional

--- src/components/Common/SectionHeader/SectionHeader.tsx
+++ src/components/Common/SectionHeader/SectionHeader.tsx
@@ -3,7 +3,7 @@ import classes from './SectionHeader.module.css';
 interface SectionHeaderProps {
   title: string;
   highlight?: string;
-  description: string;
+  description?: string;
   subDescription?: string; // 👈 new optional para
   marginBottom?: number;
 }
@@ -28,8 +28,10 @@ export default function SectionHeader({
       <h2 className={classes.sectionTitle} style={marginStyle}>
         {title}{" "}
         {highlight && <span className={classes.highlight}>{highlight}</span>}
-      </h2>
-      <p className={classes.sectionDescription} style={marginStyle}>{description}</p>
+      </h2>{
+        description && <p className={classes.sectionDescription} style={marginStyle}>{description}</p>
+      }
+      
        {subDescription && (
         <p className={classes.sectionDescription} style={marginStyle}>
           {subDescription}
