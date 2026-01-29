86e14c367727499d60ac35e4af5e38b8c2a1cfe0
Varun YourLastName <varun@example.com>
2026-01-29 11:04:22 +0530

    Industries page

---
+++ src/app/Industries/page.tsx
@@ -0,0 +1,12 @@
+import ClientSlider from '@/components/HomePage/Slider/Slider'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        <ClientSlider />
+   
+        </div>
+    
+  )
+}
