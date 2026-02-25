53f7f91c3a19591f876ec3489e597167be82b504
Varun YourLastName <varun@example.com>
2026-02-25 11:03:04 +0530

    animation_pause

--- src/components/HomePage/Slider/Slider.tsx
+++ src/components/HomePage/Slider/Slider.tsx
@@ -1,62 +1,40 @@
 "use client";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay } from 'swiper/modules';
-import Image from 'next/image';
 
-import 'swiper/css';
-import classes from './clientslider.module.css';
+import Image from "next/image";
+import classes from "./clientslider.module.css";
 
-import avanti from '../../../assets/logos/indihood_logo.png';
-import stabilitas from '../../../assets/logos/stabilitas.png';
-import tokopedia from '../../../assets/logos/tokopedia.png';
-import venzi from '../../../assets/logos/venzi.png';
+import avanti from "../../../assets/logos/indihood_logo.png";
+import stabilitas from "../../../assets/logos/stabilitas.png";
+import tokopedia from "../../../assets/logos/tokopedia.png";
+import venzi from "../../../assets/logos/venzi.png";
 
 const clientLogos = [
-  { id: 1, src: avanti, alt: 'Indihood' },
-  { id: 2, src: stabilitas, alt: 'Stabilitas' },
-  { id: 3, src: tokopedia, alt: 'Tokopedia' },
-  { id: 4, src: venzi, alt: 'Venzi' },
+  { id: 1, src: avanti, alt: "Indihood" },
+  { id: 2, src: stabilitas, alt: "Stabilitas" },
+  { id: 3, src: tokopedia, alt: "Tokopedia" },
+  { id: 4, src: venzi, alt: "Venzi" },
 ];
 
-const loopLogos = [...clientLogos,...clientLogos];
+const loopLogos = [...clientLogos, ...clientLogos,...clientLogos]; // duplicate for continuous effect
 
 export default function ClientSlider() {
   return (
     <section className={classes.wrapper}>
-  <div className={classes.container}>
-    <h4 className={classes.heading}>
-      We’ve helped 60+ global teams ship with confidence
-    </h4>
-  </div>
+      <div className={classes.container}>
+        <h4 className={classes.heading}>
+          We’ve helped 60+ global teams ship with confidence
+        </h4>
+      </div>
 
-  {/* FULL WIDTH SLIDER */}
-  <div className={classes.clientSlider}>
-    <Swiper
-      modules={[Autoplay]}
-      slidesPerView="auto"
-      spaceBetween={40}
-      loop
-      speed={5000}
-      autoplay={{
-        delay: 0,
-        disableOnInteraction: false,
-      }}
-      allowTouchMove={false}
-    >
-      {loopLogos.map((logo, index) => (
-        <SwiperSlide key={index} className={classes.clientSlide}>
-          <div className={classes.logoCard}>
-            <Image
-              src={logo.src}
-              alt={logo.alt}
-              height={40}
-              className={classes.logo}
-            />
-          </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
-  </div>
-</section>
+      <div className={classes.marquee}>
+        <div className={classes.track}>
+          {loopLogos.map((logo, index) => (
+            <div key={index} className={classes.logoCard}>
+              <Image src={logo.src} alt={logo.alt} height={40} className={classes.logo} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
   );
 }
